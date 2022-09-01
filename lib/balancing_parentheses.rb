04f371d0767e08c5e1d1454531abea4277416e86
Lucas Duncan <lucasdonuts@gmail.com>
2022-09-01 14:31:07 -0500

    commit tree de0bde56967fd678f586785271ec272c0e052641

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,27 @@
 require_relative './stack'
 
-# your code here
+def balancing_parentheses(string)
+  stack = Stack.new
+  needed = 0
+
+  string.chars.each do |c|
+    if c == "("
+      stack.push c
+      needed += 1
+    else
+      if stack.size == 0
+        needed += 1
+      else
+        stack.pop
+        needed -= 1
+      end
+    end
+  end
+
+  needed
+end
+
+# puts balancing_parentheses('(()())')
+# puts balancing_parentheses('()))')
+# puts balancing_parentheses(')))')
+# puts balancing_parentheses('(((')
